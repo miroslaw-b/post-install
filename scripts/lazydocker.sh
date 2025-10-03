eba518d34890c384dc2ee8cbc03a90fdc201ff02
Mirosław Bogacz <miroslaw.bogacz.global@gmail.com>
2025-10-03 12:00:39 +0200

    feat: lazy docker

---
+++ scripts/lazydocker.sh
@@ -0,0 +1,2 @@
+# Install lazy docker (https://github.com/jesseduffield/lazydocker#installation)
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
